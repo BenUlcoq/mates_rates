080b33d810cdab236343a7d4a595975f690b230a
Ben Ulcoq <ben.ulcoq@gmail.com>
2019-11-04 12:18:34 +1000

    Removed Validation test

--- src/mates_rates/app/models/rental.rb
+++ src/mates_rates/app/models/rental.rb
@@ -4,7 +4,7 @@ class Rental < ApplicationRecord
 
   belongs_to :tool
   belongs_to :user
-  validates :start_date, :end_date, presence: true, availability: true
+  validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
   private
